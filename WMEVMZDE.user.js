1853bf0f7b4ac4c0a05a418a2c9f5e22d3990820
Peter Olfen <peter.online.gallery@gmail.com>
2021-10-06 08:09:22 +0200

    Update WMEVMZDE.user.js

--- WMEVMZDE.user.js
+++ WMEVMZDE.user.js
@@ -2,7 +2,7 @@
 // @name WME-VMZDE
 // @description This script create buttons to open several Traffic Managemant Plattforms in Germany, using the WME paramenters where supported.
 // @namespace https://github.com/poxonline/WME-VMZDE/blob/main/WMEVMZDE.user.js
-// @version 2021.10.06.04
+// @version 2021.10.06.05
 // https://github.com/poxonline/WME-VMZDE/raw/master/WMEVMZDE.user.js
 // https://github.com/poxonline/WME-VMZDE/raw/master/WMEVMZDE.user.js
 // @include https://*.waze.com/editor*
@@ -18,7 +18,7 @@
 // 1) install this script as GitHub script
 // 2) Click on any of the links includes to open, PL Data will be handed over where supported.
 
-var vmzde_version = "2021.10.06.04";
+var vmzde_version = "2021.10.06.05";
 
 /* eslint-env jquery */ //we are working with jQuery
 //indicate used variables to be assigned
