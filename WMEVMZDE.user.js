b38d8e069a274fa12e2be2692c3c8da29ff24613
Peter Olfen <peter.online.gallery@gmail.com>
2025-08-02 00:07:02 +0200

    Update WMEVMZDE.user.js
    fourth try

--- WMEVMZDE.user.js
+++ WMEVMZDE.user.js
@@ -2,7 +2,7 @@
 // @name WME-VMZDE
 // @description This script create buttons to open several Traffic Managemant Plattforms in Germany, using the WME paramenters where supported.
 // @namespace https://github.com/poxonline/WME-VMZDE/blob/main/WMEVMZDE.user.js
-// @version 2025.08.01.03
+// @version 2025.08.01.04
 // @updateURL https://github.com/poxonline/WME-VMZDE/raw/main/WMEVMZDE.user.js
 // @downloadURL https://github.com/poxonline/WME-VMZDE/raw/main/WMEVMZDE.user.js
 // @include https://*.waze.com/editor*
@@ -18,7 +18,7 @@
 // 1) install this script as GitHub script
 // 2) Click on any of the links includes to open, PL Data will be handed over where supported.
 
-var vmzde_version = "2025.08.01.03";
+var vmzde_version = "2025.08.01.04";
 
 /* eslint-env jquery */ //we are working with jQuery
 //indicate used variables to be assigned
@@ -105,7 +105,7 @@ by_btn.click(function(){
 
     zoom = zoom > 19 ? 19 : zoom;
     zoom = zoom - 3;
-  var mapsUrl = 'https://www.bayerninfo.de/de/karte?bounds=' + lat+20 + '%2C' + lon+20 + lat-20 + '%2C' + lon-20;
+  var mapsUrl = 'https://www.bayerninfo.de/de/karte?bounds=' + lat+20 + '%2C' + lon+20 + '%2C' + lat-20 + '%2C' + lon-20 + '%2C' + &traffic=all;
        
   window.open(mapsUrl,'_blank');
 
